23b510cd4f048f4373b7aa770d63e0e2fba60eb6
Son Mai <son.mai@codegym.vn>
2020-03-12 16:06:53 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace dotnet_using_divide_by_zero_exception
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int number1 = 3000;
+            int number2 = 0;
+            try
+            {
+                int result = number1 / number2;
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
